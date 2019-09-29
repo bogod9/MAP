968754a06a35a6247cf11dcd53888ac6a36d0022
pasha308 <polina.feklenkova@yandex.by>
2019-09-29 20:21:49 +0300

    28.09.2019 Constr.L. Image

--- app/src/main/java/com/example/weather/MainActivity.java
+++ app/src/main/java/com/example/weather/MainActivity.java
@@ -2,14 +2,23 @@ package com.example.weather;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.Image;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 
+    @Override
+    public void onClick(View view) {
+
+    }
 }
