796ed43c74abe4a3a926496076232a09b28000d4
pasha308 <polina.feklenkova@yandex.by>
2019-10-02 00:07:02 +0300

    02.10.2019 00:06 dialog

--- app/src/main/java/com/example/weather/MainActivity.java
+++ app/src/main/java/com/example/weather/MainActivity.java
@@ -2,10 +2,12 @@ package com.example.weather;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
 import android.media.Image;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.content.DialogInterface;
 
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
@@ -15,10 +17,31 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
     }
 
     @Override
+
     public void onClick(View view) {
+        switch (view.getId())
+        {
+            case R.id.imageButton_refresh:
+                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                builder.setTitle("Обновление данных")
+                        .setMessage("Данные обновляются")
+                        .setCancelable(false)
+                        .setNegativeButton("ОК",
+                                new DialogInterface.OnClickListener()
+                                {
+                                    public void onClick(DialogInterface dialog, int id)
+                                    {
+                                        dialog.cancel();
+                                    }
+                                });
+                AlertDialog alert = builder.create();
+                alert.show();
+
+        }
 
     }
 }
